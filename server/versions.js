251c4d59278ca2a2917d8b1eceab93d1ce719ff7
Lukas Scheuerle <lukas.scheuerle@web.de>
2019-09-10 10:01:38 +0200

    added snapshots toggle

--- server/versions.js
+++ server/versions.js
@@ -2,7 +2,16 @@ window.addEventListener("load", getVersions);
 let versions = [];
 function displayVersions() {
     let version = document.getElementById("version");
+    let snapshot = document.getElementById("snapshots").checked;
     version.innerHTML = "";
+    if (snapshot) {
+        displaySnapshotVersions(version);
+    }
+    else {
+        displayReleaseVersions(version);
+    }
+}
+function displayReleaseVersions(_version) {
     let oldest = 9;
     let prevMajor = "";
     let optGroup;
@@ -15,12 +24,25 @@ function displayVersions() {
                 optGroup = document.createElement("optgroup");
                 optGroup.label = v[0] + "." + v[1];
                 prevMajor = v[1];
-                version.appendChild(optGroup);
+                _version.appendChild(optGroup);
             }
             let option = document.createElement("option");
             option.innerText = versions[i].id;
             option.value = versions[i].id;
             optGroup.appendChild(option);
+        }
+    }
+}
+function displaySnapshotVersions(_version) {
+    let max = 40;
+    let found = 0;
+    for (let i = 0; i < versions.length && found < max; i++) {
+        if (versions[i].type == "snapshot") {
+            let option = document.createElement("option");
+            option.innerText = versions[i].id;
+            option.value = versions[i].id;
+            _version.appendChild(option);
+            found++;
         }
     }
 }
@@ -31,6 +53,7 @@ function getVersions() {
     xhr.send();
 }
 function foundVersions(_e) {
+    document.getElementById("snapshots").addEventListener("change", displayVersions);
     let xhr = _e.target;
     if (xhr.readyState == XMLHttpRequest.DONE) {
         versions = JSON.parse(xhr.response).versions;
--- server/versions.ts
+++ server/versions.ts
@@ -1,25 +1,36 @@
 interface Version {
-  id:string,
-  type:string
+  id: string,
+  type: string
 }
 window.addEventListener("load", getVersions);
 let versions: Version[] = [];
 
 function displayVersions() {
   let version: HTMLSelectElement = <HTMLSelectElement>document.getElementById("version");
+  let snapshot: boolean = (<HTMLInputElement>document.getElementById("snapshots")).checked;
   version.innerHTML = "";
+
+  if (snapshot) {
+    displaySnapshotVersions(version);
+  } else {
+    displayReleaseVersions(version);
+  }
+
+}
+
+function displayReleaseVersions(_version: HTMLSelectElement) {
   let oldest: number = 9;
   let prevMajor: string = ""
   let optGroup: HTMLOptGroupElement;
-  for(let i:number = 0; i < versions.length; i++){
-    if(versions[i].type == "release"){
+  for (let i: number = 0; i < versions.length; i++) {
+    if (versions[i].type == "release") {
       let v: string[] = versions[i].id.split(".");
-      if(+v[1] < oldest) break;
-      if(v[1] != prevMajor){
+      if (+v[1] < oldest) break;
+      if (v[1] != prevMajor) {
         optGroup = document.createElement("optgroup");
         optGroup.label = v[0] + "." + v[1];
         prevMajor = v[1];
-        version.appendChild(optGroup);
+        _version.appendChild(optGroup);
       }
       let option: HTMLOptionElement = document.createElement("option");
       option.innerText = versions[i].id;
@@ -29,14 +40,30 @@ function displayVersions() {
   }
 }
 
-function getVersions(){
+function displaySnapshotVersions(_version: HTMLSelectElement) {
+  let max: number = 40;
+  let found: number = 0;
+  for (let i: number = 0; i < versions.length && found < max; i++) {
+    if (versions[i].type == "snapshot") {
+      let option: HTMLOptionElement = document.createElement("option");
+      option.innerText = versions[i].id;
+      option.value = versions[i].id;
+      _version.appendChild(option);
+      found++;
+    }
+  }
+}
+
+function getVersions() {
   let xhr: XMLHttpRequest = new XMLHttpRequest();
-  xhr.open("GET","https://launchermeta.mojang.com/mc/game/version_manifest.json");
-  xhr.addEventListener("readystatechange",foundVersions);
+  xhr.open("GET", "https://launchermeta.mojang.com/mc/game/version_manifest.json");
+  xhr.addEventListener("readystatechange", foundVersions);
   xhr.send();
 }
 
 function foundVersions(_e: ProgressEvent) {
+  document.getElementById("snapshots").addEventListener("change", displayVersions);
+
   let xhr: XMLHttpRequest = <XMLHttpRequest>_e.target;
   if (xhr.readyState == XMLHttpRequest.DONE) {
     versions = JSON.parse(xhr.response).versions;
